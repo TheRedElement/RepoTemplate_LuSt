9bcd871256ddc077641b27d790a0dc09aaf33221
TheRedElement <lukas.steinwender99@gmail.com>
2025-02-20 17:03:59 +1100

    added templates for python and julia

--- code/_projectbuildingblocks.jl
+++ code/_projectbuildingblocks.jl
@@ -6,4 +6,81 @@
 #%%imports
 
 #%%definitions
+"""
+    - struct description
 
+    Fields
+    ------
+        - `x1`
+            - `AbstractArray{Number,3}`, optional
+            - description
+            - the default is `Array{Number}(undef,0,0,0)`
+        - `x2`
+            - `Number`, optional
+            - description
+            - the default is `0.0`
+    Methods
+    -------
+        - 
+
+    Comments
+    --------
+"""
+struct StructTemplate{T <: Number}
+    x1::AbstractArray{T,3}
+    x2::T
+
+    #inner constructor (to allow default values)
+    function StructTemplate(
+        x1::AbstractArray{T,3}=Array{Number}(undef,0,0,0);    #positional argument
+        x2::T=0.0,                                        #named argument
+        ) where {T <: Number}
+
+        new{T}(x1, x2)
+    end
+
+end
+
+"""
+    - function description
+
+    Parameters
+    ----------
+        - `parg1`
+            - `Real`
+            - description
+        - `parg2`
+            - `Symbol`, optional
+            - description
+            - the default is `:x2` 
+        - `narg1`
+            - `Symbol`
+            - description
+        - `verbose`
+            - `Int`, optional
+            - verbosity level
+            - the default is `0`
+    
+    Raises
+    ------
+
+    Returns
+    -------
+        - `out`
+            - `Any`
+            - description
+
+    Comments
+    --------
+"""
+function function_template(
+    parg1::T,                   #positional argument
+    parg2::Symbol=:x2;          #positional argument
+    narg1::String,              #named argument
+    verbose::Int=0,             #named argument
+    )::Any where {T <: Real}
+    
+    out = nothing
+
+    return out 
+end
--- code/_projectbuildingblocks.py
+++ code/_projectbuildingblocks.py
@@ -6,6 +6,50 @@
 #%%imports
 
 #%%definitions
+class ClassTemplate:
+    """
+        - class description
+
+        Attributes
+        ----------
+            - `verbose`
+                - `int`, optional
+                - verbosity level
+                - the default is `0`
+        
+        Infered Attributes
+        ------------------
+            - 
+
+        Methods
+        -------
+            - 
+
+        Dependencies
+        ------------
+            -
+
+        Comments
+        --------
+    """
+    def __init__(self,
+        verbose:int=0
+        ) -> None:
+        return
+    
+    def __repr__(self) -> str:
+        return (
+            f'{self.__class__.__name__}(\n'
+            f'    verbose={repr(self.verbose)},\n'
+            f')'            
+        )
+    
+    def __dict__(self) -> dict:
+        return eval(str(self).replace(self.__class__.__name__, 'dict'))
+    
+    def __call__(self, *args, **kwds):
+        pass
+    
 def function_template():
     """
         - function description

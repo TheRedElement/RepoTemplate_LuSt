7101985dfad7a5bc53919d6cbb690e081a7ee1dc
Lukas Steinwender <lukas.steinwender99@gmail.com>
2025-10-31 15:53:26 +1100

    updated pbbsh to include --help and docstring

--- code/_projectbuildingblocks.sh
+++ code/_projectbuildingblocks.sh
@@ -9,41 +9,41 @@
 
 #%%definitions
 function_template() {
-    #   - function description
-    #
-    #Usage
-    #-----
-    #   ```bash
-    #   function_template [val1] [val2] [kwarg1=<kwval1>] [kwarg2=<kwval2>] [kwarg3=<kwval3>]
-    #   ```
-    #
-    #Parameters
-    #----------
-    #   - $1 (`<name>`) [optional]
-    #       - <type>
-    #       - description
-    #   - `kwarg1` [optional]
-    #       - <type>
-    #       - description
-    #   - $3+ (`arr1` `arr2` ...)
-    #       - (types) variable arguments
-    #       - description
-    #
-    #Example
-    #-------
-    #   ```bash
-    #   function_template val1 val2 kwarg1=kval1 kwarg2=kval2 kwarg3=kval3
-    #   ```
-    #
-    #Output
-    #------
-    #   ```bash
-    #   called function_template val1 val2 kwarg1=kval1 kwarg2=kval2 kwarg3=kval3
-    #   ```
-    #Comments
-    #--------
-    #
+    local help='
+        - function description
 
+        Usage
+        -----
+        ```bash
+        function_template [--help] [val1] [val2] [kwarg1=<kwval1>] [kwarg2=<kwval2>] [kwarg3=<kwval3>]
+        ```
+
+        Parameters
+        ----------
+        - $1 (`<name>`) [optional]
+            - <type>
+            - description
+        - `kwarg1` [optional]
+            - <type>
+            - description
+        - $3+ (`arr1` `arr2` ...)
+            - (types) variable arguments
+            - description
+
+        Example
+        -------
+        ```bash
+        function_template val1 val2 kwarg1=kval1 kwarg2=kval2 kwarg3=kval3
+        ```
+
+        Output
+        ------
+        ```bash
+        called function_template val1 val2 kwarg1=kval1 kwarg2=kval2 kwarg3=kval3
+        ```
+        Comments
+        --------
+    '
 
     ################
     #DEAL WITH ARGS#
@@ -68,19 +68,22 @@ function_template() {
     local arg1="${args[0]:-none}"
     local arg2="${args[0]:-none}"
 
-    ###############
-    #FUNCTION BODY#
-    ###############
+    if [[ "${arg1}" == "--help" ]]; then
+        echo "$help"
+    else
+        ###############
+        #FUNCTION BODY#
+        ###############
 
-    echo $"called function_template" \
-        "${arg1} ${arg2}" \
-        "kwarg1=${kwargs[kwarg1]}" \
-        "kwarg2=${kwargs[kwarg2]}" \
-        "kwarg3=${kwargs[kwarg3]}"
-    
+        echo $"called function_template" \
+            "${arg1} ${arg2}" \
+            "kwarg1=${kwargs[kwarg1]}" \
+            "kwarg2=${kwargs[kwarg2]}" \
+            "kwarg3=${kwargs[kwarg3]}"
+    fi
 }
 
-# #prevent direct execution
-# if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-#     echo "This is a library file. Source it in your scripts: source <path/to/_projectbuildingblocks.sh>"
-# fi
+#prevent direct execution
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    echo "This is a library file. Source it in your scripts: source <path/to/_projectbuildingblocks.sh>"
+fi

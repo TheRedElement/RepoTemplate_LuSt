c606522a4f0ccff31f30302835712b55f970e966
Lukas Steinwender <lukas.steinwender99@gmail.com>
2026-04-08 09:38:19 +1000

    adjusted pbb

--- code/_projectbuildingblocks.py
+++ code/_projectbuildingblocks.py
@@ -1,14 +1,28 @@
 
-"""
+"""building blocks used across the project
+
     - building blocks that are used in multiple files of the project
-    - use by simply importing
+    - use by importing
 
     ```python
     import importlib
-    
+
     import _projectbuildingblocks as pbb
     importlib.reload(pbb)
-    ```    
+    ```
+
+    Exceptions
+
+    Classes
+        - `ClassTemplate` -- template for class definitions
+
+    Functions
+        - `get_paths` -- extracts paths relevant for the project
+        - `function_template` -- template for function definitions
+        - `pub_fig_name` -- template for function generating publication ready figure
+        - `pub_tab_name` -- template for function generating publication ready table
+
+    Other Objects
 """
 
 #%%imports
@@ -19,130 +33,130 @@ from typing import Dict
 def get_paths(
     paths_file:str
     ) -> Dict[str,str]:
-    """
-        - function to extract paths from a bash file containing paths relevant to the project
-            - usually `_path.sh`
-        - does so by
-            - reading the file
-            - extracting groups that match r"([^=\n]+)=([^=\n]+)"
-            - converting matches to `dict`
-        
-        Parameters
-        ----------
-            - `paths_file`
-                - `str`
-                - path to `_paths.sh` file
-                    - file containing project paths
-        
-        Raises
-        ------
+    """extracts paths relevant for the project
 
-        Returns
-        -------
-            - `paths`
-                - `Dict[str,str]`
-                - dictionary mapping `path_name` (bash variable name) to `path` (absolute path)
+    - function to extract paths from a bash file containing paths relevant to the project
+        - usually `_path.sh`
+    - does so by
+        - reading the file
+        - extracting groups that match r"([^=\n]+)=([^=\n]+)"
+        - converting matches to `dict`
+    - usually called at the start of a python script to ensure all the paths are loaded
 
-        Dependencies
-        ------------
-            - `re`
-            - `typing`
+    Parameters
+        - `paths_file`
+            - `str`
+            - path to `_paths.sh` file
+                - file containing project paths
 
-        Comments
-        --------
-            - usually called at the start of a python script to ensure all the paths are loaded
+    Raises
+
+    Returns
+        - `paths`
+            - `Dict[str,str]`
+            - dictionary mapping `path_name` (bash variable name) to `path` (absolute path)
+
+    Dependencies
+        - `re`
+        - `typing`
+
     """
     with open(paths_file, "r") as f:
         paths = dict(re.findall(r"([^=\n]+)=([^=\n]+)", f.read()))
     return paths
 
 class ClassTemplate:
-    """
-        - class description
+    """class representing ...
 
-        Attributes
-        ----------
-            - `verbose`
-                - `int`, optional
-                - verbosity level
-                - the default is `0`
-        
-        Infered Attributes
-        ------------------
-            - 
+    <long description>
 
-        Methods
-        -------
-            - 
+    Attributes
+        - `a` [-- short description]
+        - ...
 
-        Dependencies
-        ------------
-            -
+	Inferred Attributes
+        - `c`
+			- `type` [, optional]
+			- <description>
+			- [the default is <default>]
+        - ...
 
-        Comments
-        --------
+    Methods
+        - `add()` -- <short description>
+
+    Dependencies
+        -
     """
     def __init__(self,
-        verbose:int=0
         ) -> None:
-        return
-    
+        """constructor
+
+        <long description>
+
+        Parameters
+            - `param_name`
+                - `type` [, optional]
+                - <description>
+                - [the default is <default>]
+            - ...
+
+        Raises
+
+        Returns
+        """
+        pass
+
     def __repr__(self) -> str:
-        return (
-            f'{self.__class__.__name__}(\n'
-            f'    verbose={repr(self.verbose)},\n'
-            f')'            
-        )
-    
+        """returns string representation of the class"""
+        return f"{self.__class__.__name__}(" + ", ".join([f"{attr}={val}" for attr, val in self.__dict__.items()]) + ")"
+
     def __dict__(self) -> dict:
         return eval(str(self).replace(self.__class__.__name__, 'dict'))
-    
-    def __call__(self, *args, **kwds):
+
+    def __call__(self, *args, **kwargs):
         pass
 
 def function_template():
-    """
-        - function description
+    """returns ...
 
-        Parameters
-        ----------
-            - `param_name`
-                - `type`, <optional>
-                - description
-                - the default is `<default>`
+    <long description>
 
+	Parameters
+		- `param_name`
+			- `type` [, optional]
+			- <description>
+			- [the default is <default>]
+		- ...
 
-        Raises
-        ------
-            - `error_type`
-                - 
+	Raises
+		- `SomeException`
+			- description
 
-        Returns
-        -------
-            - `varable_name`
-                - `type`, <optional>
-                - description
-                
-        Dependencies
-        ------------
+	Returns
+		- `return_name`
+			- `type`
+			- <description>
+		- ...
 
-        Comments
-        --------
+	Dependencies
+	    -
     """
     return
 
 
-def pub_fig_name(args):
-    """
-        - function to generate a figure that is finetuned for publication (talk/paper/poster)
+def pub_fig_name(*args):
+    """generates figure for ...
+
+    - function to generate a figure that is fine-tuned for publication (talk/paper/poster)
     """
 
     return
-    
-def pub_tab_name(args):
-    """
-        - function to generate a table that is finetuned for publication (talk/paper/poster)
+
+def pub_tab_name(*args):
+    """generates table for ...
+
+    - function to generate a table that is fine-tuned for publication (talk/paper/poster)
     """
 
     return
-    
+
